4879c4f9806e47d1fa5b7ea5b82d41f501fafe42
Mohammad Rahman <me@shadikur.com>
2023-05-07 22:13:43 +0200

    Add coffee updated

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,7 +1,130 @@
 const AddCoffee = () => {
+  const handleAddCoffee = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const quantity = form.quantity.value;
+    const supplier = form.supplier.value;
+    const photo = form.photo.value;
+
+    const coffeData = {
+      name,
+      quantity,
+      supplier,
+      photo,
+    };
+    console.log(coffeData);
+
+    //   Send data to server
+    fetch("http://localhost:5003/coffee", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(coffeData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
   return (
     <div>
-      <h1>Add a coffee</h1>
+      <section className="py-10 bg-gray-100 sm:py-16 lg:py-24">
+        <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+          <div className="max-w-2xl mx-auto text-center">
+            <h2 className="text-3xl font-bold leading-tight text-gray-900 sm:text-4xl lg:text-5xl">
+              Add a Coffee
+            </h2>
+          </div>
+          <div className="max-w-5xl mx-auto mt-12 sm:mt-16">
+            <div className="mt-6 overflow-hidden bg-white rounded-xl">
+              <div className="px-6 py-12 sm:p-12">
+                <form onSubmit={handleAddCoffee} className="mt-14">
+                  <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-5 gap-y-4">
+                    <div>
+                      <label
+                        htmlFor=""
+                        className="text-base font-medium text-gray-900"
+                      >
+                        Coffee Name
+                      </label>
+                      <div className="mt-2.5 relative">
+                        <input
+                          type="text"
+                          name="name"
+                          id="name"
+                          placeholder="Enter the name of a coffee"
+                          className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <label
+                        htmlFor=""
+                        className="text-base font-medium text-gray-900"
+                      >
+                        Available Quantity
+                      </label>
+                      <div className="mt-2.5 relative">
+                        <input
+                          type="text"
+                          name="quantity"
+                          id="quantity"
+                          placeholder="Enter the quantity"
+                          className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <label
+                        htmlFor=""
+                        className="text-base font-medium text-gray-900"
+                      >
+                        Supplier
+                      </label>
+                      <div className="mt-2.5 relative">
+                        <input
+                          type="text"
+                          name="supplier"
+                          id="supplier"
+                          placeholder="Supplier of the coffee"
+                          className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <label
+                        htmlFor=""
+                        className="text-base font-medium text-gray-900"
+                      >
+                        Photo URL
+                      </label>
+                      <div className="mt-2.5 relative">
+                        <input
+                          type="text"
+                          name="photo"
+                          id="photo"
+                          placeholder="Enter photo URL"
+                          className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600"
+                        />
+                      </div>
+                    </div>
+                    <div className="sm:col-span-2">
+                      <button
+                        type="submit"
+                        className="inline-flex items-center justify-center w-full px-4 py-4 mt-2 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md focus:outline-none hover:bg-blue-700 focus:bg-blue-700"
+                      >
+                        Add Coffee
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
